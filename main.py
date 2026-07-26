bb3d34733aee85eff658d6819108e07a81fbbf52
ianshajupadhyay <anshajupadhyay1999@gmail.com>
2026-07-26 14:57:43 +0530

    Implement data transformation layer with validation checks

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from python.ingestion.loader import load_raw_data
 from python.ingestion.validator import validate_raw_data
+from python.transformation.transform import transform_raw_data
 
 def main():
     validate_raw_data()
     raw_data = load_raw_data()
-    print(f"Loaded {len(raw_data)} datasets")
+    print(f"Loaded {len(raw_data)} raw datasets")
+    transformed_data = transform_raw_data(raw_data)
+    for name, data in transformed_data.items():
+        print(f"transformed {name}")
+    
 
 if __name__ == "__main__":
     main()
